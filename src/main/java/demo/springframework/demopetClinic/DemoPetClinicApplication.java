6ed439280c56b2e23343ddd3ff63ed670b9f6b9f
ztexdee <tap879@gmail.com>
2021-11-11 15:34:28 +0300

    project initialized

---
+++ src/main/java/demo/springframework/demopetClinic/DemoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package demo.springframework.demopetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/springframework/demopetClinic/DemoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.springframework.demopetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
