71f4d40c2e8b4655a42d7c45d28c63011acfdb6a
ztexdee <tap879@gmail.com>
2021-11-16 13:30:13 +0300

    dummy commit

--- pet-clinic-web/src/main/java/demo/springframework/demopetclinic/DemoPetClinicApplication.java
+++ pet-clinic-web/src/main/java/demo/springframework/demopetclinic/DemoPetClinicApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoPetClinicApplication {
-
+//dummy comment
 	public static void main(String[] args) {
 		SpringApplication.run(DemoPetClinicApplication.class, args);
 	}
