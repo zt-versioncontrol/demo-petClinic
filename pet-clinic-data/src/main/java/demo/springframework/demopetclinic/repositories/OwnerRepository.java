693c3c8bc0a6c5a35c0efc7457cc7dff942c12fb
ztexdee <tap879@gmail.com>
2022-01-07 22:07:14 +0300

    Added service for Owner. Closes #40.

--- pet-clinic-data/src/main/java/demo/springframework/demopetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/demo/springframework/demopetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import demo.springframework.demopetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
