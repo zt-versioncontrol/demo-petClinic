3b59d31e7d948174b2478d4959537c20ec381356
ztexdee <tap879@gmail.com>
2022-01-19 23:50:56 +0300

    Refactored using Lombok. Closes #51

--- pet-clinic-data/src/main/java/demo/springframework/demopetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/demo/springframework/demopetclinic/model/Owner.java
@@ -1,12 +1,26 @@
 package demo.springframework.demopetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
+
+    @Builder
+    public Owner(Long id, String firsrtName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firsrtName, lastName);
+        Address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 
     @Column(name = "address")
     private String Address;
@@ -19,36 +33,4 @@ public class Owner extends Person{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String address) {
-        Address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
--- pet-clinic-data/src/main/java/demo/springframework/demopetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/demo/springframework/demopetclinic/model/Vet.java
@@ -1,24 +1,28 @@
 package demo.springframework.demopetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
+
+    @Builder
+    public Vet(Long id, String firstName, String lastName, Set<Specialty> specialties) {
+        super(id, firstName, lastName);
+        this.specialties = specialties;
+    }
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties",
         joinColumns = @JoinColumn(name = "vet_id"),
         inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
-
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
